fd900420e0118183d89da964b7cce37e3a6ea45b
Duyb <1255248666@qq.com>
2021-08-15 21:11:18 +0800

    Strapi 与 Gridsome 集成：将 Strapi 数据**预取**到 Gridsome 中
    @gridsome/source-strapi

--- gridsome.config.js
+++ gridsome.config.js
@@ -15,6 +15,21 @@ module.exports = {
         path: './content/blog/**/*.md',
       },
     },
+    {
+      use: '@gridsome/source-strapi',
+      options: {
+        apiURL: 'http://localhost:1337',
+        queryLimit: 1000, // Defaults to 100
+        contentTypes: ['post'],
+        // singleTypes: ['impressum'],
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // loginData: {
+        //   identifier: '',
+        //   password: '',
+        // },
+      },
+    },
   ],
   templates: {
     Post: [
